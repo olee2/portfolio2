d09d202b268afbacfe340007a0b1b4f617d1e472
olee2 <oleaeik@protonmail.com>
2023-05-31 08:31:33 +0200

    Portfolio beginning

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,160 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import CameraIcon from "@mui/icons-material/PhotoCamera";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      Oscar Elny {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const cards = [1, 2, 3];
+
+const defaultTheme = createTheme({
+  typography: {
+    fontFamily: "Poppins, sans-serif",
+  },
+  palette: {
+    text: { primary: "#39577b" },
+    primary: {
+      main: "#39577b",
+      text: "#39577b",
+    },
+    secondary: {
+      main: "#7A4945",
+    },
+  },
+});
+
+export default function Portfolio() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="md">
+            <Typography
+              component="h1"
+              variant="h3"
+              align="start"
+              color="text.primary"
+              gutterBottom
+            >
+              Oscar's Portfolio
+            </Typography>
+            <Typography
+              variant="h5"
+              align="start"
+              color="text.secondary"
+              paragraph
+            >
+              I'm a front-end developer with a knack for turning coffee into
+              code. Proficient in HTML, CSS, JavaScript, and React, I create
+              pixel-perfect web experiences. Together, let's build something
+              that'll make users forget to blink!
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container
+          sx={{ py: 8, flex: 1, display: "flex", alignItems: "center" }}
+          maxWidth="lg"
+        >
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: "56.25%",
+                    }}
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: "background.paper", p: 3 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    name: "The Auction House",
+    type: "Auction Site",
+    description: `My second year semester project at Noroff. An auction site where a user can register, login, make bids and put objects up for auction. The API for the project was provided by Noroff.`,
+    site_url: "https://the-auction-house.netlify.app/",
+    tech: ["html", "css", "js", "bootstrap"],
+    git_url: "https://github.com/olee2/auction-house",
+    img: "../assets/auction.png",
+  },
+  {
+    name: "Ecom",
+    type: "E-commerce Site",
+    description:
+      "This was the course assignment for the JS Frameworks course at Noroff. A simple e-commerce site built in react. API with products provided by Noroff.",
+    site_url: "https://ecom-a1b.pages.dev/",
+    git_url: "https://github.com/olee2/ecom",
+    img: "../assets/ecom.png",
+  },
+  {
+    name: "Holidaze",
+    type: "Accommodations Site",
+    description:
+      "My second year exam project at Noroff. A accommodation site where a user or venue manager can register, login, and book or publish venues. API provided by Noroff.",
+    site_url: "https://noroff-csm.netlify.app/index.html",
+    git_url: "https://github.com/olee2/Noroff-CSM",
+    img: "../assets/holidaze.png",
+  },
+];
